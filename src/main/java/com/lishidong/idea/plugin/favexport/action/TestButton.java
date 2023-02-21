b7c018e183fcad49c3fb1be4f1c35de2b40d0be7
lishidongol <996479960@qq.com>
2023-02-21 20:16:51 +0800

    初始化项目

---
+++ src/main/java/com/lishidong/idea/plugin/favexport/action/TestButton.java
@@ -0,0 +1,12 @@
+package com.lishidong.idea.plugin.favexport.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class TestButton extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
